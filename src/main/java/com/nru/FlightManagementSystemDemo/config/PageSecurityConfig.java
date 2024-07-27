e10a2f59cf56ad944102a5785f6ae84135e9b6cc
nru707 <nsdanu707@gmail.com>
2024-07-27 18:04:13 +0530

    commit

--- src/main/java/com/nru/FlightManagementSystemDemo/config/PageSecurityConfig.java
+++ src/main/java/com/nru/FlightManagementSystemDemo/config/PageSecurityConfig.java
@@ -63,8 +63,7 @@ public class PageSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()).authorizeRequests(requests -> requests.antMatchers("/Admin/**").hasRole("ADMIN")
-                .antMatchers("/customer/**").hasRole("CUSTOMER")
-                .antMatchers("/**").permitAll())
+                .antMatchers("/customer/**").hasRole("CUSTOMER").antMatchers("/**").permitAll())
                 .formLogin(login -> login.loginPage("/Login").loginProcessingUrl("/Login").successHandler(successHandler).permitAll())
                 .logout(logout -> logout.addLogoutHandler(logOutHandler).logoutUrl("/Logout"));
 	}
